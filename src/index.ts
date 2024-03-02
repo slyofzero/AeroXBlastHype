0e569bdd874b3443c4d580a88665bd2c1a14e4aa
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-03-02 21:27:39 +0530

    New Dexscreener headers

--- src/index.ts
+++ src/index.ts
@@ -16,14 +16,14 @@ const headers = {
   Pragma: "no-cache",
   "Cache-Control": "no-cache",
   "User-Agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0",
   Upgrade: "websocket",
   Origin: "https://dexscreener.com",
   "Sec-WebSocket-Version": "13",
   "Accept-Encoding": "gzip, deflate, br",
   "Accept-Language": "en-US,en;q=0.9",
-  "Sec-WebSocket-Key": "oEhi9FPY7B8xeU+7/6jGyw==",
-  "Sec-WebSocket-Extensions": "permessage-deflate; client_max_window_bits",
+  "Sec-WebSocket-Key": "MG9trx/r3u4CkbuquwU4PQ==",
+  "Sec-WebSocket-Extensions": "permessage-deflate",
 };
 
 (async function () {
